9bae85e2db80b89851031d486796c5c0ba004432
Merle Tyson Mills <tysonmills5@gmail.com>
2021-10-22 12:47:11 -0400

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "react-bootstrap/Button"
+
+function Header() {
+    return (
+        <>
+            <h1>Super Cool Header</h1>
+            <Button>Button</Button>
+        </>
+    )
+};
+
+export default Header;
