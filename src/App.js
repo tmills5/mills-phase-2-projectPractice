e55d1793b49ff7a7583fa3d2cebc2e052a8f9961
Merle Tyson Mills <tysonmills5@gmail.com>
2022-03-09 11:14:02 -0600

    reset

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header';
-// import Navbar from './Components/Navbar';
-// import Form from './Components/Form';
-import ViewJson from './Components/ViewJson';
+
 
 
 function App() {
 
 
-
-
   return (
-    <div className="container">
-      <Header />
-      {/* <Navbar /> */}
-      {/* <Form /> */}
-      <ViewJson />
+    <div>
+    
     </div>
   );
 }
